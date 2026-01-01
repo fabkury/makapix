182c47f456c7a25e6442d8921fe351afb2551ab9
Fabrício Kury <fab@kury.dev>
2026-01-01 15:08:47 +0000

    Add favicon and app icons, and include web manifest
    - Added multiple icon files for different resolutions: android-chrome-192x192.png, android-chrome-512x512.png, apple-touch-icon.png, favicon-16x16.png, favicon-32x32.png, and favicon.ico.
    - Created a site.webmanifest file to define application metadata, including icons and theme colors.
    - Updated the _document.tsx file to link the new favicon and manifest files for improved branding and user experience.

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -38,6 +38,13 @@ export default function Document() {
 })();`,
           }}
         />
+        {/* Favicons */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
         {/* Preconnect to Google Fonts for faster loading */}
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
